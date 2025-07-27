c3070e189fe22351d1b959ec0c75b3e56e0531d3
mohammed musharaf ali <musharafrcr@gmail.com>
2025-07-27 14:54:03 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Wellcome to NIT
+
+# # Numbers
+
+# In[1]:
+
+
+15
+
+
+# In[2]:
+
+
+5
+
+
+# In[3]:
+
+
+4+5  // addition
+
+
+# In[4]:
+
+
+65*54  // multiplication
+
+
+# In[5]:
+
+
+45-54  //substraction
+
+
+# In[6]:
+
+
+65/8   // float division
+
+
+# In[7]:
+
+
+65//8  // int dividion
+
+
+# # string (text)
+
+# In[8]:
+
+
+wellcome to fsds class
+
+
+# In[9]:
+
+
+'wellcome to fsds class'
+
+
+# In[10]:
+
+
+"wellcome to fsds class"
+
+
+# In[11]:
+
+
+"wellcome to
+fsds class"
+
+
+# In[12]:
+
+
+"wellcome to
+fsds class'''
+
+
+# In[13]:
+
+
+'''wellcome to fsds class"
+
+
+# In[14]:
+
+
+'''wellcome to 
+fsds class'''
+
+
+# # Python variable creation 
+
+# - variable name = value
+
+# In[2]:
+
+
+v=9
+
+
+# In[6]:
+
+
+v
+
+
+# In[7]:
+
+
+id(v)
+
+
+# In[8]:
+
+
+9 = v
+
+
+# - rules to define python variable
+
+# - 1) variable name never starts withthe numbers
+
+# In[9]:
+
+
+9v=8
+
+
+# In[10]:
+
+
+v9 = 8
+v9
+
+
+# In[11]:
+
+
+var=20
+
+
+# In[12]:
+
+
+VAR
+
+
+# - 2.variables are case sensitive
+
+# In[13]:
+
+
+var
+
+
+# In[14]:
+
+
+nit=7
+nareshit
+
+
+# - 3.CALL THE VARIIABLE WITH THE SAME NAME AND IT IS DEFINED 
+
+# In[16]:
+
+
+pushpa1 , pushpa2 = 200
+
+
+# In[19]:
+
+
+pushpa1,pushpa2=2000,1800
+
+
+# In[21]:
+
+
+pushpa1
+pushpa2
+
+
+# In[22]:
+
+
+print(pushpa1)
+print(pushpa2)
+
+
+# In[23]:
+
+
+nit$=7
+
+
+# - 4.special characters will not be able to define as variable
+
+# - 5.keywords or reserved words cannot be defined as variable
+
+# - python ha 35 reserved words 
+
+# In[24]:
+
+
+if=43
+
+
+# In[25]:
+
+
+import keyword
+keyword.kwlist
+
+
+# - there are the all 35 keywords of python 
+
+# In[ ]:
+
+
+
+
